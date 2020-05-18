7970f51c309c905de87f8d6bd1d1945b0d9c438e
avsuv <suverin.av@gmail.com>
2020-05-18 15:43:32 +0300

    Задание 5

--- 3/5.py
+++ 3/5.py
@@ -1,16 +1,21 @@
-def summary(str1, pos):
+def summary(str1):
     global full_sum
-    for el in str1[:pos]:
-        full_sum += int(el)
+    for el in str1:
+        full_sum += el
     return full_sum
 
 
-full_sum = 0
+full_sum = 0    # Сумма аргументов
 while True:
     my_str = input('Введите числа через пробел и нажмите Enter. Для завершения, введите "*": ').split()
     if '*' in my_str:
-        q_pos = my_str.index('*')
-        print('Сумма:', summary(my_str, q_pos))
+        q_pos = my_str.index('*')   # Узнаем позицию спецсимвола
+        my_str = list(map(float, my_str[:q_pos]))   # Переводим значения до спецсимвола во float
+        print('Сумма:', summary(my_str))
         break
     else:
-        print('Сумма:', summary(my_str, len(my_str)))
+        try:
+            my_str = list(map(float, my_str))   # Если введено не число и не спецсимвол, обрабатываем ошибку
+            print('Сумма:', summary(my_str))
+        except:
+            print('При вводе допущена ошибка!')
