992f542aa621ab0f2619995a9ba4a5d23531b4b0
avsuv <suverin.av@gmail.com>
2020-05-26 11:42:54 +0300

    Задание 4

---
+++ 5/4.py
@@ -0,0 +1,20 @@
+numbers = []
+with open('2.txt') as file:
+    for line in file:
+        numbers.append(line.strip().split(' '))
+
+for el in numbers:
+    if el[0] == 'One':
+        el[0] = 'Один'
+    elif el[0] == 'Two':
+        el[0] = 'Два'
+    elif el[0] == 'Three':
+        el[0] = 'Три'
+    elif el[0] == 'Four':
+        el[0] = 'Четыре'
+print(numbers)
+
+with open('2_new.txt', 'w', encoding='utf-8') as file:
+    for line in numbers:
+        a = ' '.join(line)
+        file.write(a + '\n')
