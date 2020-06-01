b31e8d28d06f28ac73cba301f11336ec64d40047
avsuv <suverin.av@gmail.com>
2020-06-02 00:27:57 +0300

    Задание 1

--- 7/1.py
+++ 7/1.py
@@ -15,7 +15,7 @@ class Matrix:
         l1 = len(self.matrix_in[0])
         h2 = len(other.matrix_in)
         l2 = len(other.matrix_in[0])
-        matrix_new = [[0 for _ in range(max(h1, h2))] for el in range(max(l1, l2))] # Пустая матрица, заполненная нулями
+        matrix_new = [[0 for _ in range(max(l1, l2))] for el in range(max(h1, h2))] # Пустая матрица, заполненная нулями
         for i in range(h1):     # Сначала складываем нулевую матрицу с первой
             for j in range(l1):
                 matrix_new[i][j] += self.matrix_in[i][j]
@@ -31,6 +31,12 @@ matr_1 = Matrix([[31, 22],
 
 matr_2 = Matrix([[3, 5, 32],
                  [2, 4, 6]])
+
+matr_3 = Matrix([[1, 2, 3, 4]])
 print(matr_1)
 print(matr_2)
+print(matr_3)
 print(matr_1 + matr_2)
+print(matr_1 + matr_3)
+matr_4 = matr_2 + matr_3
+print(matr_4)
